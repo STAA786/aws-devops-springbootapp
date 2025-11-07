fc7de48b2810a37c0b8a7dc0ae5ba7cdcbee8b5a
SIDDIQUI TAA <86817779+STAA786@users.noreply.github.com>
2025-11-07 17:01:09 +0530

    Update SpringbootAwsDeployApplicationTest.java

--- src/test/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplicationTests.java
+++ src/test/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplicationTests.java
@@ -1,12 +1,20 @@
 package com.example.springbootawsdeploy;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.function.Function;
 
-@SpringBootTest
-class SpringbootAwsDeployApplicationTests {
+@SpringBootApplication
+public class SpringbootAwsDeployApplication {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAwsDeployApplication.class, args);
+    }
+
+    @Bean
+    public Function<String, String> hello() {
+        return input -> "Hello from Lambda, input = " + input;
     }
 }
+

9d047f1332870433d875df42484be7c6ec6a8daf
SIDDIQUI TAA <86817779+STAA786@users.noreply.github.com>
2025-11-07 17:01:53 +0530

    Update SpringbootAwsDeployApplication.java

--- src/main/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplication.java
+++ src/main/java/com/example/springbootawsdeploy/SpringbootAwsDeployApplication.java
@@ -3,7 +3,6 @@ package com.example.springbootawsdeploy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import java.util.function.Function;
 
 @SpringBootApplication
